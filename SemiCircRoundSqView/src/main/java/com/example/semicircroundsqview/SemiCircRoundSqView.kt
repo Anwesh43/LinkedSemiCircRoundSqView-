527177797eeac86492b53b8e5ddf330861f14203
Anwesh43 <anweshthecool0@gmail.com>
2020-12-07 02:42:39 +0530

    adding Renderer class

--- SemiCircRoundSqView/src/main/java/com/example/semicircroundsqview/SemiCircRoundSqView.kt
+++ SemiCircRoundSqView/src/main/java/com/example/semicircroundsqview/SemiCircRoundSqView.kt
@@ -185,4 +185,27 @@ class SemiCircRoundSqView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SemiCircRoundSqView) {
+
+        private val animator : Animator = Animator(view)
+        private val scrs : SemiCircRoundSq = SemiCircRoundSq(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            scrs.draw(canvas, paint)
+            animator.animate {
+                scrs.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            scrs.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
