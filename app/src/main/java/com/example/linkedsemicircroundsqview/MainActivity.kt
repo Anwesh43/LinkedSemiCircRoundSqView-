dacb16cc2af9ac83a82a42b2d313629b4c3f8482
Anwesh43 <anweshthecool0@gmail.com>
2020-12-07 02:52:53 +0530

    adding view in acgtivity and fullscreen support

--- app/src/main/java/com/example/linkedsemicircroundsqview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsemicircroundsqview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsemicircroundsqview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.semicircroundsqview.SemiCircRoundSqView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SemiCircRoundSqView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
