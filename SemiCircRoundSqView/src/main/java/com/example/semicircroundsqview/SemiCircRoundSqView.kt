a924d0e64b2036262b8956cf51a92bd89eb1f972
Anwesh43 <anweshthecool0@gmail.com>
2020-12-07 01:33:23 +0530

    adding constants and libraries

--- SemiCircRoundSqView/src/main/java/com/example/semicircroundsqview/SemiCircRoundSqView.kt
+++ SemiCircRoundSqView/src/main/java/com/example/semicircroundsqview/SemiCircRoundSqView.kt
@@ -1,2 +1,26 @@
 package com.example.semicircroundsqview
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#2196F3",
+    "#FF5722",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+
